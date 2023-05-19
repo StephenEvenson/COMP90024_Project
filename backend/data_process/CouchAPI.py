f387d4e2ed27b4cc66af7e111b97a54be7e41e31
Jarvis <shenjiahao.20000115@gmail.com>
2023-05-19 20:20:16 +1000

    map reduce init

--- backend/data_process/CouchAPI.py
+++ backend/data_process/CouchAPI.py
@@ -4,7 +4,6 @@ import decimal
 import requests
 
 
-
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
@@ -12,11 +11,13 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(obj)
 
 
-class myDB(couchdb.Database):
-    def __int__(self, url, name=None, session=None):
+class Mydb(couchdb.Database):
+    def __init__(self, url, name=None, session=None, map_reduce_functions=None):
         super().__init__(url, name, session)
+        self.map_reduce_functions = map_reduce_functions
 
     def save_batch(self, data):
+        """Save a batch of documents to the database."""
         json_body = json.dumps({'docs': data}, cls=DecimalEncoder)
         url = f'{self.resource.url}/_bulk_docs'
         requests.post(
@@ -28,11 +29,50 @@ class myDB(couchdb.Database):
             auth=self.resource.credentials
         )
 
+    def create_view(self, view_name):
+        """Create a view in the database."""
+        # Load the MapReduce functions from the JSON file
+        with open('map_reduce_functions.json', 'r') as file:
+            map_reduce_functions = json.load(file)
 
-class CouchApi(couchdb.Server):
-    def __init__(self, server_url='http://localhost:5984/', username='admin', password='admin'):
+        # Get the specified view's map and reduce functions
+        map_func = map_reduce_functions[view_name]['map']
+        reduce_func = map_reduce_functions[view_name]['reduce']
+
+        design_doc_id = "_design/" + view_name
+
+        # If the design document already exists, get it
+        if design_doc_id in self:
+            design_doc = self[design_doc_id]
+        else:
+            # Otherwise, create a new design document
+            design_doc = {'views': {}}
+
+        # Update the design document with the new view
+        design_doc['views'][view_name] = {
+            'map': map_func,
+            'reduce': reduce_func
+        }
+
+        # Save the design document
+        self[design_doc_id] = design_doc
+
+    def query_data_from_view(self, view_name, limit=None, skip=None):
+        """Query the database with the specified view."""
+        # Query the view and return the results
+        rows = self.view(view_name + '/' + view_name, group=True, limit=limit, skip=skip)
+        return [(row.key, row.value) for row in rows]
+
+
+class CouchAPI(couchdb.Server):
+    def __init__(self, server_url='http://localhost:5984/', username='admin', password='admin',
+                 map_reduce_file='map_reduce_functions.json'):
         super().__init__(server_url)
         self.resource.credentials = (username, password)
+
+        # Load the MapReduce functions from the JSON file
+        with open(map_reduce_file, 'r') as file:
+            self.map_reduce_functions = json.load(file)
 
     def __getitem__(self, name):
         """Return a `Database` object representing the database with the
@@ -43,6 +83,6 @@ class CouchApi(couchdb.Server):
         :rtype: `Database`
         :raise ResourceNotFound: if no database with that name exists
         """
-        db = myDB(self.resource(name), name)
+        db = Mydb(self.resource(name), name, map_reduce_functions=self.map_reduce_functions)
         db.resource.head()  # actually make a request to the database
         return db
