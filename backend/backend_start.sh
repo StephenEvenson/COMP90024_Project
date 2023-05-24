1d62a8a426b3326f772fdff92ed9573752baf5ba
StephenEveson <1258455347@qq.com>
2023-05-24 19:00:29 +1000

    update backend

--- backend/backend_start.sh
+++ backend/backend_start.sh
@@ -3,7 +3,7 @@
 echo "start backend server"
 while :
   do
-    /data/app/wait-for-it.sh "$WRITE_DB_PORT":"$WRITE_DB_PORT" --timeout=5
+    /data/app/wait-for-it.sh "$WRITE_DB_HOST":"$WRITE_DB_PORT" --timeout=5
     # shellcheck disable=SC2181
     if [ $? -eq 0 ]; then
       # shellcheck disable=SC2039
