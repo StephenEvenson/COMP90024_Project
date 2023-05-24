3426ead267b1881b8427660c4efc36c88d5f4971
rachel <jieshen0403@gmail.com>
2023-05-24 16:49:51 +1000

    update backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+import os
 
 from database.couch_api import DatabaseService
 
 app = FastAPI()
 
-db_service = DatabaseService(server_url='http://192.168.0.80:5984/', username='admin', password='admin')
+read_db_host = os.environ.get('READ_DB_HOST')
+read_db_port = os.environ.get('READ_DB_PORT')
+write_db_port = os.environ.get('WRITE_DB_PORT')
+write_db_host = os.environ.get('WRITE_DB_HOST')
+
+read_db_service = DatabaseService(server_url=f'http://{read_db_host}:{read_db_port}/', username='admin', password='admin')
+write_db_service = DatabaseService(server_url=f'http://{write_db_host}:{write_db_port}/', username='admin', password='admin')
 
 
 @app.get("/")
@@ -19,60 +26,60 @@ async def say_hello(name: str):
 
 @app.get("/api/views/create/{db_name}")
 async def create_views(db_name: str):
-    db_service.create_views(db_name)
+    write_db_service.create_views(db_name)
     return {"message": "views created"}
 
 
 @app.get("/api/mastodon/new/{source}/{seconds}")
 async def get_new_mastodon_data(source: str, seconds: int):
-    docs = db_service.get_mastodon_new_data('mastodon', source, seconds)
+    docs = read_db_service.get_mastodon_new_data('mastodon', source, seconds)
     return {"message": "new mastodon data", "docs": docs}
 
 
 @app.get("/api/mastodon/sentiment/{seconds}")
 async def get_mastodon_sentiment_data(seconds: int):
-    docs = db_service.get_mastodon_sentiment_data('mastodon', seconds)
+    docs = read_db_service.get_mastodon_sentiment_data('mastodon', seconds)
     return {"message": "mastodon sentiment", "docs": docs}
 
 
 @app.get("/api/sudo/regional_language")
 async def get_sudo_regional_language():
-    docs = db_service.get_sudo_regional_language('sudo_regional_language')
+    docs = read_db_service.get_sudo_regional_language('sudo_regional_language')
     return {"message": "sudo regional language", "docs": docs}
 
 
 @app.get("/api/sudo/regional_population")
 async def get_sudo_regional_population():
-    docs = db_service.get_sudo_regional_population('sudo_regional_population')
+    docs = read_db_service.get_sudo_regional_population('sudo_regional_population')
     return {"message": "sudo regional population", "docs": docs}
 
 
 @app.get("/api/sudo/gcc_homeless")
 async def get_sudo_gcc_homeless():
-    docs = db_service.get_sudo_gcc_homeless('sudo_gcc_homeless')
+    docs = read_db_service.get_sudo_gcc_homeless('sudo_gcc_homeless')
     return {"message": "sudo gcc homeless", "docs": docs}
 
 
 @app.get("/api/sudo/sudo_sa4_homeless")
 async def get_sudo_sa4_homeless():
-    docs = db_service.get_sudo_sa4_homeless('sudo_sa4_homeless')
+    docs = read_db_service.get_sudo_sa4_homeless('sudo_sa4_homeless')
     return {"message": "sudo sa4 homeless", "docs": docs}
 
 
 @app.get("/api/sudo/sudo_sa4_income")
 async def get_sudo_sa4_income():
-    docs = db_service.get_sudo_sa4_income('sudo_sa4_income')
+    docs = read_db_service.get_sudo_sa4_income('sudo_sa4_income')
     return {"message": "sudo sa4 income", "docs": docs}
 
 
 @app.get("/api/sudo/init")
 async def init_sudo():
-    db_service.init_sudo()
+    write_db_service.init_sudo()
     return {"message": "sudo initialized"}
 
 
 @app.get("/api/twitter/count/{scenario}")
-async def init_huge(scenario: str):
+async def init_twitter(scenario: str):
     # scenario = 'all' | 'homeless' | 'language' | 'abuse'
     if scenario == 'all':
         return {
