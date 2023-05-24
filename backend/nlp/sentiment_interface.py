7d0f8c54d989b477c5b7aeb71e9d762a6dead8d8
Juntao Lu <ralph0813.ljt@gmail.com>
2023-05-24 10:50:04 +1000

    update data process

--- backend/nlp/sentiment_interface.py
+++ backend/nlp/sentiment_interface.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 sentiment_model_name = 'cardiffnlp/twitter-roberta-base-sentiment'
 sentiment_model = AutoModelForSequenceClassification.from_pretrained(sentiment_model_name)
@@ -10,14 +10,14 @@ sentiment_tokenizer = AutoTokenizer.from_pretrained(sentiment_model_name)
 
 
 def get_sentiment_score(text: str) -> float:
-    inputs = sentiment_tokenizer(text, return_tensors="pt")
+    inputs = sentiment_tokenizer(text, return_tensors="pt").to(device)
     outputs = sentiment_model(**inputs)
     probs = outputs.logits.softmax(dim=1).detach()
     return probs[0][1].item()
 
 
 def get_sentiment_scores(texts: list[str]) -> list[float]:
-    inputs = sentiment_tokenizer(texts, return_tensors="pt", padding=True, truncation=True, max_length=256)
+    inputs = sentiment_tokenizer(texts, return_tensors="pt", padding=True, truncation=True, max_length=256).to(device)
     outputs = sentiment_model(**inputs)
     probs = outputs.logits.softmax(dim=1).detach()
     return probs[:, 2].tolist()
