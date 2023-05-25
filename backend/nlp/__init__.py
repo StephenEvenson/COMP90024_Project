f3144179899f38115425b03e01054c9a8d77e732
StephenEveson <1258455347@qq.com>
2023-05-26 08:07:56 +1000

    update comment

--- backend/nlp/__init__.py
+++ backend/nlp/__init__.py
@@ -1,3 +1,18 @@
+#
+# Part of Assignment 2 - COMP90024
+#
+# Cluster and Cloud Computing - Team 72
+#
+# Authors:
+#
+#  - Juntao Lu (Student ID: 1290513)
+#  - Runtian Zhang (Student ID: 1290379)
+#  - Jiahao Shen (Student ID: 1381187)
+#  - Yuchen Liu (Student ID: 1313394)
+#  - Jie Shen (Student ID: 1378708)
+#
+# Location: Melbourne
+#
 from .abusive_interface import get_abusive_score, get_abusive_scores
 from .sentiment_interface import get_sentiment_score, get_sentiment_scores
 from .search_interface import compute_embedding, get_cross_score, retrieve, re_rank_topk, re_rank_thresh, \
--- backend/nlp/abusive_interface.py
+++ backend/nlp/abusive_interface.py
@@ -1,3 +1,18 @@
+#
+# Part of Assignment 2 - COMP90024
+#
+# Cluster and Cloud Computing - Team 72
+#
+# Authors:
+#
+#  - Juntao Lu (Student ID: 1290513)
+#  - Runtian Zhang (Student ID: 1290379)
+#  - Jiahao Shen (Student ID: 1381187)
+#  - Yuchen Liu (Student ID: 1313394)
+#  - Jie Shen (Student ID: 1378708)
+#
+# Location: Melbourne
+#
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
